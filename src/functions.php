d67167c72f0d9e9f8be9e05408b866e59013e426
brotzky <brotzky@gmail.com>
2016-12-10 19:46:21 -0800

    building out functions

--- src/functions.php
+++ src/functions.php
@@ -47,3 +47,150 @@ function vanhack_widgets_init() {
   ) );
 }
 add_action( 'widgets_init', 'vanhack_widgets_init' );
+
+/**
+ * vanchack functions and definitions.
+ *
+ * @link https://codex.wordpress.org/Functions_File_Explained
+ *
+ * @package vanchack
+ */
+
+ /**
+  * Importing MetaBox Settings
+  * @link https://wordpress.org/plugins/meta-box/
+  */
+ // require_once(TEMPLATEPATH.'/lib/meta-box.php');
+
+ // Contains all the AJAX calls to the DB
+ // require_once(TEMPLATEPATH.'/lib/ajax.php');
+ // Contains all the Custom Post Types
+ // require_once(TEMPLATEPATH.'/custom-functions/custom-post-types.php');
+ // Contains all the overrides of default WP behaviour
+ require_once(TEMPLATEPATH.'/custom-functions/custom-overrides.php');
+ // Contains all the Custom Widgets
+ // require_once(TEMPLATEPATH.'/custom-functions/custom-widgets.php');
+ // Contains all the various functions that do not belong in the ones above
+ // require_once(TEMPLATEPATH.'/custom-functions/custom-functions.php');
+
+
+
+if ( ! function_exists( 'vanhack_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function vanhack_setup() {
+  /*
+   * Make theme available for translation.
+   * Translations can be filed in the /languages/ directory.
+   * If you're building a theme based on vanchack, use a find and replace
+   * to change 'vanchack' to the name of your theme in all the template files
+   */
+  load_theme_textdomain( 'vanchack', get_template_directory() . '/languages' );
+
+  // Add default posts and comments RSS feed links to head.
+  add_theme_support( 'automatic-feed-links' );
+
+  /*
+   * Let WordPress manage the document title.
+   * By adding theme support, we declare that this theme does not use a
+   * hard-coded <title> tag in the document head, and expect WordPress to
+   * provide it for us.
+   */
+  add_theme_support( 'title-tag' );
+
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
+
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => esc_html__( 'Primary Menu', 'vanchack' ),
+        'mobile' => esc_html__( 'Mobile Menu', 'vanchack'),
+        'footer_copyright' => esc_html__( 'Footer Copyright', 'vanchack')
+    ));
+
+  /*
+   * Switch default core markup for search form, comment form, and comments
+   * to output valid HTML5.
+   */
+  add_theme_support( 'html5', array(
+    'search-form',
+    'comment-form',
+    'comment-list',
+    'gallery',
+    'caption',
+  ) );
+
+  /*
+   * Enable support for Post Formats.
+   * See https://developer.wordpress.org/themes/functionality/post-formats/
+   */
+  add_theme_support( 'post-formats', array(
+    'aside',
+    'image',
+    'video',
+    'quote',
+    'link',
+  ) );
+
+  // Set up the WordPress core custom background feature.
+  add_theme_support( 'custom-background', apply_filters( 'vanhack_custom_background_args', array(
+    'default-color' => 'ffffff',
+    'default-image' => '',
+  ) ) );
+}
+endif; // vanhack_setup
+add_action( 'after_setup_theme', 'vanhack_setup' );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function vanchack_widgets_init() {
+  register_sidebar( array(
+    'name'          => esc_html__( 'Sidebar', 'vanchack' ),
+    'id'            => 'sidebar-1',
+    'description'   => '',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h2 class="widget-title">',
+    'after_title'   => '</h2>',
+  ) );
+}
+add_action( 'widgets_init', 'vanchack_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function vanchack_scripts() {
+
+  include(TEMPLATEPATH . '/custom-functions/custom-styles.php');
+
+  include(TEMPLATEPATH . '/custom-functions/custom-scripts.php');
+
+}
+add_action( 'wp_enqueue_scripts', 'vanchack_scripts' );
+
+/**
+ * Add event categories column to events index page in admin
+ */
+add_action( 'manage_posts_custom_column', 'price_column_display', 10, 2);
+// Allow editing of authors in custom post type
+function allowAuthorEditing() {
+  add_post_type_support( 'vanchack', 'author' );
+}
+add_action('init','allowAuthorEditing');
+
+/**
+ * END of WP default functions code
+ */
+
