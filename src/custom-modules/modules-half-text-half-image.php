399eba866d5e13c9b63ca815fabc7a248bb71cb6
brotzky <brotzky@gmail.com>
2017-01-14 17:13:12 -0800

    Creating blue bg and multi side half text half image module

--- src/custom-modules/modules-half-text-half-image.php
+++ src/custom-modules/modules-half-text-half-image.php
@@ -1,29 +1,50 @@
 <?php if('half_text_half_image' === get_row_layout() ): 
 
+  $bg_color    = get_sub_field('bg_color');
   $header      = get_sub_field('header');
   $text        = get_sub_field('text');
   $cta_text    = get_sub_field('cta_text');
   $cta_link    = get_sub_field('cta_link');
   $image       = get_sub_field('image');
-
+  $image_side  = get_sub_field('image_side');
 ?>
 
-<section class="HalfTextHalfImageModule">
-  <div class="Container">
-    <div class="HalfTextHalfImage">
-      <div class="HalfTextHalfImage__image">
-        <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']; ?>">
+    <section class="HalfTextHalfImageModule <?php if($bg_color === 'light_blue') : echo 'vanhack-bg'; endif; ?>">
+    <?php if($image_side === 'left' ): ?>
+      <div class="Container">
+        <div class="HalfTextHalfImage">
+          <div class="HalfTextHalfImage__image">
+            <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']; ?>">
+          </div>
+          <div class="HalfTextHalfImage__content">
+            <hr class="TextRule">
+            <div class="HalfTextHalfImage__header"><h3><?php echo $header; ?></h3></div>
+            <div class="HalfTextHalfImage__text"><p><?php echo $text; ?></p></div>
+            <div class="HalfTextHalfImage__cta">
+              <a class="ButtonSecondary" href="<?php echo $cta_link ?>"><?php echo $cta_text ?></a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="HalfTextHalfImage__content">
-        <hr class="TextRule">
-        <div class="HalfTextHalfImage__header"><h3><?php echo $header; ?></h3></div>
-        <div class="HalfTextHalfImage__text"><p><?php echo $text; ?></p></div>
-        <div class="HalfTextHalfImage__cta">
-          <a class="ButtonSecondary" href="<?php echo $cta_link ?>"><?php echo $cta_text ?></a>
+  <?php endif; ?>
+
+  <?php if($image_side === 'right' ): ?>
+    <div class="Container">
+      <div class="HalfTextHalfImage">
+        <div class="HalfTextHalfImage__content">
+          <hr class="TextRule">
+          <div class="HalfTextHalfImage__header"><h3><?php echo $header; ?></h3></div>
+          <div class="HalfTextHalfImage__text"><p><?php echo $text; ?></p></div>
+          <div class="HalfTextHalfImage__cta">
+            <a class="ButtonSecondary" href="<?php echo $cta_link ?>"><?php echo $cta_text ?></a>
+          </div>
+        </div>
+        <div class="HalfTextHalfImage__image">
+          <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']; ?>">
         </div>
       </div>
     </div>
-  </div>
-</section>
+  <?php endif; ?>
+  </section>
 
 <?php endif; ?>
