ad8f9da256ace58bff35d41b541f0d78952c87e8
brotzky <brotzky@gmail.com>
2017-01-14 18:15:02 -0800

    Creating 2 column row FAQ questions

--- src/custom-modules/modules-faq.php
+++ src/custom-modules/modules-faq.php
@@ -1,9 +1,16 @@
 <?php if( 'faq_module' === get_row_layout()):
 
+  $i = 0;
+
   if( have_rows('faq_repeater') ): ?>
 
   <section class="FaqModule">
     <div class="Container">
+    <div class="SectionHeader SectionHeader--center">
+      <hr class="TextRule">
+      <div class="HalfTextHalfImage__header"><h3>Frequently Asked Questions</h3></div>
+    </div>
+
       <?php while( have_rows('faq_repeater') ): the_row();
 
         // Repeater Variables
@@ -11,7 +18,12 @@
         $faq_answer   = get_sub_field('faq_answer');
 
       ?>
-      <div class="FaqRow">
+
+      <?php if( $i % 2 == 0 or $i == 0 ): ?>
+        <div class="Faq__row">
+      <?php  endif; ?>
+
+      <div class="Faq__answer">
 
         <?php if( $faq_question ): ?>
         <h3 class="Faq__header"><?php echo $faq_question; ?></h3>
@@ -22,10 +34,19 @@
         <?php endif; ?>
 
       </div>
-      <?php endwhile; ?>
+
+      <?php if( $i % 2 == 1): ?>
+        </div>
+      <?php endif; ?>
+
+      <?php
+        $i++;
+        endwhile; ?>
+
     </div>
   </section>
 
   <?php endif; ?>
 
 <?php endif; ?>
+
