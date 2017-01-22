29160678bfc63fdf0084f6f8eeb554bf25168331
brotzky <brotzky@gmail.com>
2017-01-21 22:13:55 -0800

    Startup Accelerator module

---
+++ src/custom-modules/modules-startup-accelerator.php
@@ -0,0 +1,20 @@
+<?php if('startup_accelerator' === get_row_layout() ):?>
+
+<section class="StartupAcceleratorModule">
+  <div class="Container">
+    <div class="SectionHeader SectionHeader--center">
+      <hr class="TextRule">
+      <div class="HalfTextHalfImage__header"><h3>VanHack Supporters</h3></div>
+    </div>
+    <div class="StartupAccelerator__logos">
+      <a target="_blank" href="http://www.techstars.com/">
+        <img src="/wp-content/themes/VanHack/build/img/modules/startup-accelerators/techstars-logo.png" alt="VanHack Techstars Berline">
+      </a>
+      <a target="_blank" href="http://www.startupchile.org/">
+        <img src="/wp-content/themes/VanHack/build/img/modules/startup-accelerators/startup-chili-logo.png" alt="VanHack Startup Chili">
+      </a>
+    </div>
+  </div>
+</section>
+
+<?php endif; ?>
