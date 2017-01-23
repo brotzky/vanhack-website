f6ae479c43bd78c65bd29f7ff0b882460ffd285f
brotzky <brotzky@gmail.com>
2017-01-22 16:57:26 -0800

    Adding text to Cities module

--- src/custom-modules/modules-cities.php
+++ src/custom-modules/modules-cities.php
@@ -1,13 +1,19 @@
 <?php if('cities_module' === get_row_layout()): ?>
   <section class="CitiesModule">
     <div class="Container--large">
+      <div class="Container">
+        <div class="CitiesHeader SectionHeader">
+          <hr class="TextRule">
+          <div class="Cities__header"><h3>Get hired faster with VanHack's global reach</h3></div>
+          <div class="Cities__subtitle">Get access to professional development, english practice, and more to help you land your dream job.</div>
+        </div>
+      </div>
       <div class="CitiesRoot">
         <div class="Cities__image">
           <img src="/wp-content/themes/vanhack/img/modules/cities/cities-modules-city-mosaic.jpg" alt="Vanhack connecting cities">
         </div>
         <div class="Cities__tiled-card">
           <div class="Cities__content">
-            <h2 class="Cities__header">VanHack all over the world.</h2>
             <ul class="Cities__list">
               <li class="Cities__item">Berlin</li>
               <li class="Cities__item">Amsterdam</li>
